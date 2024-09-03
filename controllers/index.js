241f70c62018cb886921889dc4d8b41d7cb403f7
akaufmanfrey <akaufmanfrey@gmail.com>
2024-09-03 10:55:51 -0400

    moved create post to dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 // Import all of the routes from controllers here
 const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api/");
-const pageOneRoutes = require("./pageOneRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 // Connect the routes to the router here
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/pageOne", pageOneRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
